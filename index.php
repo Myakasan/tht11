dc36f24083146573539d18aa1a1c317ef40bf54d
Etienne Mya K <egendron6@icloud.com>
2018-11-30 10:22:17 +0100

    Ajout main.css et h1

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
   <head>
     <meta charset="utf-8">
     <title>Température</title>
+    <link rel="stylesheet" href="./style/main.css">
   </head>
   <body>
+    <h1>Température</h1>
     <?php
       echo "hello world";
      ?>
