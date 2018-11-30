eb65b4f86b0abb97f717dee788f3374bfc6aacfc
Etienne Mya K <egendron6@icloud.com>
2018-11-30 09:53:44 +0100

    first page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Temp</title>
+  </head>
+  <body>
+    <?php
+      echo "hello world";
+     ?>
+  </body>
+</html>
